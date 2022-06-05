70c906339aa6144704688b3cae810216fb991377
Emilia Ashraghi <emiliaashraghi@gmail.com>
2022-06-04 21:49:42 -0400

    php

--- send.php
+++ send.php
@@ -13,7 +13,7 @@ $mensaje .= "Su e-mail es: " . $mail . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['project'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'ignaciobluuweb@gmail.com';
+$para = 'emiliaashraghi@gmail.com';
 $asunto = 'Mensaje de mi sitio web';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
