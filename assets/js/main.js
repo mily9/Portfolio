d1fcbec858b5429be334cfe14319802c3d3bfef2
Emilia Ashraghi <emiliaashraghi@gmail.com>
2022-06-26 20:34:33 -0400

    fd

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,29 +6,6 @@ function scrollHeader(){
 }
 window.addEventListener('scroll', scrollHeader)
 
-/*=============== SERVICES MODAL ===============*/
-const modalViews = document.querySelectorAll('.services__modal'),
-      modalBtns = document.querySelectorAll('.services__button'),
-      modalClose = document.querySelectorAll('.services__modal-close')
-
-      let modal = function(modalClick){
-        modalViews[modalClick].classList.add('active-modal')
-      }
-
-      modalBtns.forEach((mb, i) => {
-        mb.addEventListener('click', () =>{
-          modal(i)
-        })
-      })
-
-      modalClose.forEach((mc) => {
-        mc.addEventListener('click', () => {
-          modalViews.forEach((mv) => {
-            mv.classList.remove('active-modal')
-          })
-        })
-      })
-
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 let mixerPortfolio = mixitup(".work__container", {
   selectors: {
@@ -140,4 +117,3 @@ sr.reveal(`.home__handle`, {delay: 700})
 sr.reveal(`.home__social, .home__scroll`, {delay: 900, origin: 'bottom'})
 
 
-/*=============== FORM ===============*/
